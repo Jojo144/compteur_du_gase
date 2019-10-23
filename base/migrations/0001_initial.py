feb917b087631a5e4e8d3861629983beb135bc5c
zoa2944 <zoa2944@orange.com>
2019-10-23 09:49:39 +0200

    [commun] Modification de la taille d'un champ.

--- base/migrations/0001_initial.py
+++ base/migrations/0001_initial.py
@@ -188,7 +188,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('date', models.DateTimeField(auto_now=True)),
                 ('amount', models.DecimalField(decimal_places=2, max_digits=15)),
-                ('kind', models.CharField(choices=[('cash', 'Espèces'), ('cheque', 'Chèque'), ('cancellation', 'Annulation/Correction'), ('repayment', 'Remboursement'), ('ontheflight', 'A la volée')], default='cash', max_length=6, null=True)),
+                ('kind', models.CharField(choices=[('cash', 'Espèces'), ('cheque', 'Chèque'), ('cancellation', 'Annulation/Correction'), ('repayment', 'Remboursement'), ('ontheflight', 'A la volée')], default='cash', max_length=12, null=True)),
                 ('household', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='base.Household')),
             ],
             options={
