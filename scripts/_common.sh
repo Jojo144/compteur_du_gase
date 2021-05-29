0fb4d96458cfbbc42925a55896eb7bbeb88debea
Jojo144 <jojo144@girole.fr>
2021-05-29 15:01:01 +0200

    Fudion install scripts

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkg_dependencies="python3-pip python3-virtualenv python3-venv python3-wheel sqlite3"
+pkg_dependencies="nginx python3-pip python3-virtualenv python3-venv python3-wheel sqlite3"
 
 handle_is_home_public() {
     if [[ $is_home_public -eq 1 ]]; then
