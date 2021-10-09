c6d2f788c38d5b65d1aca498a48868b6e7c195c2
Jojo144 <jojo144@girole.fr>
2021-10-09 19:56:56 +0200

    Typo

--- base/views.py
+++ base/views.py
@@ -61,7 +61,7 @@ def index(request):
                    'txt_message': txt_message,
                    'activity_board': local_settings.activity_board,
                    'activity_list': activity_list,
-                   'use_logo': local_settings.use_logo)
+                   'use_logo': local_settings.use_logo})
 
 
 def gestion(request):
