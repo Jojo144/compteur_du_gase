bfea18de4f1ac22ac642769070c4d755ae7921f9
Jocelyn Delalande <jocelyn@crapouillou.net>
2022-01-15 21:18:13 +0100

    Ajoute un bouton de logout

--- compteur/urls.py
+++ compteur/urls.py
@@ -22,6 +22,7 @@ from compteur.settings import PATH
 urlpatterns = [
     path(PATH, include('base.urls')),
     path(PATH + 'admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.YNH_INTEGRATION_ENABLED:
