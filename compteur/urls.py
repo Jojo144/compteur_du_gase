d9f444b7d3e497714e221d8acb021fbbe592120a
Jojo144 <jojo144@girole.fr>
2022-01-15 14:09:05 +0100

    Remove useless import

--- compteur/urls.py
+++ compteur/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
 
 from compteur.settings import PATH
 
