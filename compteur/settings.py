68c060d7e7fd8d90636622ce43c179db767dfddf
Jocelyn Delalande <jocelyn@crapouillou.net>
2022-01-16 01:33:20 +0100

    Met à jour les dépendances (dont Django en 3.2)

--- compteur/settings.py
+++ compteur/settings.py
@@ -20,6 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 
+# Silence models.W042 warning (stick to 32 bits)
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 # Application definition
 
 INSTALLED_APPS = [
