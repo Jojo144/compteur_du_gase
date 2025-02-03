e7172eadaf4a7512f2670085efbdda489680e650
Jojo144 <jojo144@girole.fr>
2025-02-03 22:12:50 +0100

    Change CustomHeaderMiddleware to YNH_USER

--- ynh_auth/middleware.py
+++ ynh_auth/middleware.py
@@ -1,4 +1,8 @@
 from django.contrib.auth.middleware import RemoteUserMiddleware
 
 class CustomHeaderMiddleware(RemoteUserMiddleware):
-    header = 'HTTP_REMOTE_USER'  # nom du header dans les requêtes qui contient le nom d'utilisateur courant
+    header = 'HTTP_YNH_USER'  # nom du header dans les requêtes qui contient le nom d'utilisateur courant
+
+    def process_request(self, request):
+        print (str(request))
+        super().process_request(request)
