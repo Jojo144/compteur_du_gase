c3c9ff66ac55962575c12ce5fafe9792b9dced64
Antoine Gaillard <antoine.gaillard@lilo.org>
2025-06-22 17:08:41 +0200

    Affichage plus explicite des alerts stock dans liste produits

--- base/views.py
+++ base/views.py
@@ -367,23 +367,32 @@ class ProductsListView(ListView):
     def get_context_data(self, **kwargs):
         local_settings = get_local_settings()
 
-        pdts = [
-            {
-                "id": p.id,
-                "nom": p.name,
-                "catégorie": str(p.category),
-                "fournisseur": str(p.provider),
-                "prix d'achat": '{} € / {}'.format(p.cost_of_purchase, p.unit.name),
-                "prix de vente": '{} € / {}'.format(p.price, p.unit.name),
-                "visible": bool_to_utf8(p.visible),
-                "vrac": bool_to_utf8(p.unit.vrac),
-                "alerte stock": '{} [{}]'.format(bool_to_utf8(p.stock < p.stock_alert), round_stock(p.stock_alert)) if (p.stock_alert) else '',
-                "stock": round_stock(p.stock),
-                "details_url": reverse("base:detail_product", args=(p.id,)),
-                "stats_url": reverse("base:stats", args=(p.id,)),
-            }
-            for p in self.get_queryset()
-        ]
+        pdts = []
+
+        for p in self.get_queryset():
+            isAlertDefined = bool(p.stock_alert)
+            isInAlert = (p.stock < p.stock_alert) if isAlertDefined else False
+            alertSymbol = "⚠️" if isInAlert else "✅"
+            comparator = "<" if isInAlert else ">"
+            alerteStockInfo = '{} ({} {})'.format(alertSymbol, comparator, round_stock(p.stock_alert)) if isAlertDefined else ''
+
+            pdts.append(
+                {
+                    "id": p.id,
+                    "nom": p.name,
+                    "catégorie": str(p.category),
+                    "fournisseur": str(p.provider),
+                    "prix d'achat": '{} € / {}'.format(p.cost_of_purchase, p.unit.name),
+                    "prix de vente": '{} € / {}'.format(p.price, p.unit.name),
+                    "visible": bool_to_utf8(p.visible),
+                    "vrac": bool_to_utf8(p.unit.vrac),
+                    "is_in_alert": isInAlert,
+                    "alerte stock": alerteStockInfo,
+                    "stock": round_stock(p.stock),
+                    "details_url": reverse("base:detail_product", args=(p.id,)),
+                    "stats_url": reverse("base:stats", args=(p.id,)),
+                }
+            )
 
         columns = self.get_columns(local_settings)
 
